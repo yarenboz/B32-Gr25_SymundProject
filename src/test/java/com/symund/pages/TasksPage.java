67017b91b27d292c672ea08d1eeab49328630865
Anasben00 <bendwamas@gmail.com>
2024-03-19 17:02:20 -0400

    feature file created

--- src/test/java/com/symund/pages/TasksPage.java
+++ src/test/java/com/symund/pages/TasksPage.java
@@ -1,4 +1,26 @@
 package com.symund.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static com.symund.utilities.Driver.getDriver;
+
 public class TasksPage extends BasePage{
+
+    public TasksPage() {
+        PageFactory.initElements(getDriver(),this);
+    }
+
+    @FindBy (id = "notes_new_note")
+    public WebElement NewNote;
+
+    @FindBy (id = "notes_new_note")
+    public WebElement NewNote;
+
+    @FindBy (id = "notes_new_note")
+    public WebElement NewNote;
+
+    @FindBy (id = "notes_new_note")
+    public WebElement NewNote;
 }
--- src/test/resources/features/NotesModule_Anas.feature
+++ src/test/resources/features/NotesModule_Anas.feature
@@ -4,6 +4,7 @@ Feature: Users should be able to login
   Background: User is already in the login page
     Given the user is on the login page
     And the user logged in with username as "Employee125" and password as "Employee123"
+    And the user navigates to "Notes" module
 
   @wip
   Scenario: Verify login with valid credentials
